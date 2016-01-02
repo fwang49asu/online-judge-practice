57a1f88eb37238f19013b5fb5d932ab57a96b804
FengWang <fwang49@asu.edu>
2016-01-02 15:41:57 -0700

    leetcode 3

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 using namespace std;
 
 class Solution {
   public:
     int lengthOfLongestSubstring(string s) {
-      unordered_map<char, int> index_map;
-      if(s.length() == 0) {
-        return 0;
-      }
-      int result = 1;
-      int start = 0;
-      index_map.insert(make_pair(s[0], 0));
-      for(int end = 1; end < s.length(); ++end) {
-        char c = s[end];
-        if(index_map.find(c) != index_map.end()) {
-          start = max(start, index_map[c]+1);
+        int result = 1;
+        int start = 0;
+        map<char, int> index_map;
+        index_map[s[0]] = 0;
+        for(int end = 1; end < s.length(); ++end) {
+            char c = s[end];
+            if(index_map.end() != index_map.find(c)) {
+                start = max(start, index_map[c]+1);
+            }
+            index_map[c] = end;
+            result = max(result, end-start+1);
         }
-        result = max(result, end - start + 1);
-        index_map[c] = end;
-      }
-      return result;
+        return result;
     }
 };
 
