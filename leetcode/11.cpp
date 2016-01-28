c8ec243a2f2f75ed74e725f06408d462cf8f97f2
FengWang <fwang49@asu.edu>
2016-01-28 00:22:47 -0700

    leetcode 11

---
+++ leetcode/11.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int result = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        for(; left < right; ) {
+            result = max(result, (right - left) * min(height[left], height[right]));
+            if(height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return result;
+    }
+};
