e92e0930e5ff293264236406bd1e8090ed27c5ad
FengWang <fwang49@asu.edu>
2016-08-24 22:22:09 -0700

    leetcode 92 ac

--- leetcode/92.cpp
+++ leetcode/92.cpp
@@ -6,7 +6,31 @@ struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
+    ~ListNode() {
+        if(next != NULL) {
+            delete next;
+        }
+    }
 };
+
+ListNode* createList(int n) {
+    ListNode* head = NULL;
+    while(n >= 1) {
+        ListNode* p = new ListNode(n);
+        p->next = head;
+        head = p;
+        --n;
+    }
+    return head;
+}
+
+void print(ListNode* head) {
+    while(head != NULL) {
+        cout << head->val << ' ';
+        head = head->next;
+    }
+    cout << endl;
+}
 
 class Solution {
     public:
@@ -19,33 +43,58 @@ class Solution {
             p->next = head;
             head = p;
             ListNode* pre[2];
-            ListNode* post[2];
-            ListNode* current[2];
             --m;
             --n;
             pre[0] = pre[1] = head;
-            for(int i=0;i<m; ++m) {
+            for(int i=0; i<m; ++i) {
                 pre[0] = pre[0]->next;
             }
-            for(int i=0;i<n; ++m) {
+            for(int i=0; i<n; ++i) {
                 pre[1] = pre[1]->next;
             }
-            while((n-m) > 1) {
-                current[0] = pre[0]->next;
-                current[1] = pre[1]->next;
-                post[0] = current[0]->next;
-                post[1] = current[0]->next;
-                current[0]->next = post[1];
-                current[1]->next = post[0];
-                pre[0]->next = current[1];
 
-                
-                ++m;
-                --n;
-            }
+            ListNode* after = pre[1]->next->next;
+            pre[1]->next->next = NULL;
+
+            ListNode* last = pre[0]->next;
+            pre[0]->next = reverse(last);
+            last->next = after;
+
             // chop off the dummy head
             p = head->next;
+            head->next = NULL;
             delete head;
             return p;
         }
+        ListNode* reverse(ListNode* head) {
+            ListNode* p = new ListNode(0);
+            p->next = head;
+
+            ListNode* pre = p;
+            ListNode* cur = pre->next;
+            ListNode* next = cur->next;
+
+            while(next != NULL) {
+                cur->next = pre;
+                pre = cur;
+                cur = next;
+                next = next->next;
+            }
+            cur->next = pre;
+
+            head->next = NULL;
+            p->next = NULL;
+            delete p;
+            return cur;
+        }
+};
+
+int main() {
+    int n;
+    ListNode* head = createList(3);
+    print(head);
+    Solution s;
+    head = s.reverseBetween(head, 2, 3);
+    print(head);
+    delete head;
 }
