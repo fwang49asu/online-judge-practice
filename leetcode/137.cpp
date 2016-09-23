e10bf9c1bc7155bdd0e4af34dfaeb64db45e17d6
Feng Wang <fwang49@asu.edu>
2016-09-23 10:30:45 -0700

    leetcode 137 add comment

--- leetcode/137.cpp
+++ leetcode/137.cpp
@@ -3,6 +3,13 @@ public:
     int singleNumber(vector<int>& nums) {
         int one = 0;
         int two = 0;
+
+        // similiar to find the unique number in "double numbers"
+        // a a a b:
+        // a 0
+        // 0 a
+        // 0 0
+        // b 0
         for(int x : nums) {
             one = (one ^ x) & ~two;
             two = (two ^ x) & ~one;
