4722a325eff3770d77a8b041ca6babb8fd3109bf
FengWang <fwang49@asu.edu>
2016-10-12 02:38:44 -0700

    leetcode 258 ac

---
+++ leetcode/258.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0) {
+            return 0;
+        }
+        int n = num % 9;
+        return n == 0 ? 9 : n;
+    }
+};
