268bf70244f2acf744f166173af96e156e2a4a4c
FengWang <fwang49@asu.edu>
2016-12-06 23:44:18 -0700

    324 O(n)

--- leetcode/324.cpp
+++ leetcode/324.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> temp = nums;
-        sort(temp.begin(), temp.end());
         int n = nums.size();
-        int low = (n + 1) / 2;
-        int high = n;
-        for(int i=0; i<n; ++i) {
-            nums[i] = ((i & 1) == 1) ? temp[--high] : temp[--low];
+        int high = n - 1;
+        int low = 0;
+        int index = 0;
+        auto midp = nums.begin() + (n / 2);
+        nth_element(nums.begin(), midp, nums.end());
+        int mid = *midp;
+        while(index <= high) {
+            int vIndex = virtualIndex(index, n);
+            int vHigh = virtualIndex(high, n);
+            int vLow = virtualIndex(low, n);
+            if(nums[vIndex] > mid) {
+                swap(nums[vIndex], nums[vLow]);
+                ++index;
+                ++low;
+            } else if(nums[vIndex] < mid) {
+                swap(nums[vIndex], nums[vHigh]);
+                --high;
+            } else {
+                ++index;
+            }
         }
+    }
+private:
+    int virtualIndex(int i, int n) {
+        return (1 + i * 2) % (n | 1);
     }
 };
