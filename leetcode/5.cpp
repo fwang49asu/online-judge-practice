aba567e95b883c001380f1531daf28945a675f26
Feng Wang <fwang49@asu.edu>
2019-01-07 14:52:27 +0800

    Update 5.cpp

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -1,35 +1,35 @@
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if(s == "") {
-            return "";
-        }
-        int maxlength = 1;
-        int minstart = 0;
-        for(int i=0; i<s.length();) {
-            int low = i;
-            int high = low + 1;
-            while(s[low] == s[high]) {
-                ++high;
-            }
-            int next = high;
-            --high;
-            while(low >=0 && high < s.length() && s[low] == s[high]) {
-                int length = high - low + 1;
-                if(length > maxlength) {
-                    maxlength = length;
-                    minstart = low;
-                }
-                --low;
-                ++high;
-            }
-            i = next;
-        }
-        return s.substr(minstart, maxlength);
+  string longestPalindrome(string s) {
+    if (s.length() == 0) {
+      return "";
     }
-
+    int n = s.length();
+    
+    int maxLength = 1;
+    int maxStart = 0;
+    for (int i = 0; i < n;) {
+      int low = i;
+      int high = i + 1;
+      // initial expansion
+      while (high < n && s[i] == s[high]) {
+        ++high;
+      }
+      int next = high;
+      --low;
+      while (low >= 0 && high < n && s[low] == s[high]) {
+        --low;
+        ++high;
+      }
+      ++low;
+      --high;
+      int length = high - low + 1;
+      if (length > maxLength) {
+        maxLength = length;
+        maxStart = low;
+      }
+      i = next;
+    }
+    return s.substr(maxStart, maxLength);
+  }
 };
